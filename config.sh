dbbdabc6b2940a1ffb2c855f51e901cc6ff22d86
piux2 <>
2023-07-04 20:18:32 -0700

    added config file

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Please use a separate admin account than test1 ( g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5 )
+# DO NOT use test1 ( g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5 ) in production. Mnemonic code is published on github.
+# adminkey's address should match with the admin address in faucet.gno
+# admin key to add controllers in the faucet
+adminkey='test1'
+
+# funding key to provide funding for controllers, admin and faucet contracts
+fundingkey='test1'
+
+# chain rpc endpoint
+rpc='test3.gno.land:36657'
+
+# chain id
+chainid='dev'
